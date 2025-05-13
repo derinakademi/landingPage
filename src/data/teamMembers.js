5cf50006aa3df7b0c3a229509e0d81a238c93ac0
Nerffer <burakeroglu2002@hotmail.com>
2025-05-13 23:04:47 +0300

    header'da yer alan "DERİN AKADEMİ" yazısı link yapıldı

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -1,6 +1,6 @@
 export const teamMembers = [
     { name: "Ersin Çine", image: "/images/takım-img/ersin.jpeg", bgColor: "#00C7BE" },
-    { name: "Gökhan Karahanoğulları", image: "/images/takım-img/gökhan.jpeg", bgColor: "#F73751" },
+    { name: "Gökhan Karahanoğulları", image: "/images/takım-img/gokhan.jpeg", bgColor: "#F73751" },
     { name: "Emre Cem Dönmez", image: "/images/takım-img/emre.jpeg", bgColor: "#2FB6F5" },
     { name: "Burak Eroğlu", image: "/images/takım-img/burak.jpeg", bgColor: "#FF9000" },
     { name: "Mehmet Sarper Kahvecioğlu", image: "/images/takım-img/sarper.jpeg", bgColor: "#FFCC00" },
